9698dfec598693a21bb05514a59891dff962629e
Igor <iigormazetti@hotmail.com>
2023-02-15 16:11:53 -0300

    iniciando o projeto

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,27 @@
+import { StatusBar } from "expo-status-bar";
+import { Text, View, TextInput, TouchableOpacity } from "react-native";
+import { styles } from "./styles";
+
+export default function Home() {
+  const handleAddParticipant = () => {
+    alert("clicou");
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.texto}>Nome do evento</Text>
+      <Text style={styles.subTexto}>Quarta, 15 de Fevereiro de 2023</Text>
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder="Nome do participante"
+          placeholderTextColor="#6B6B6B"
+        />
+        <TouchableOpacity style={styles.addBtn} onPress={handleAddParticipant}>
+          <Text style={styles.btnText}>+</Text>
+        </TouchableOpacity>
+      </View>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,45 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#242424",
+    padding: 24,
+  },
+  texto: {
+    color: "#FFF",
+    fontSize: 24,
+    marginTop: 48,
+  },
+  subTexto: {
+    color: "#6B6B6B",
+    fontSize: 18,
+  },
+  input: {
+    flex: 1,
+    backgroundColor: "#1F1E25",
+    height: 56,
+    borderRadius: 5,
+    color: "#FFF",
+    padding: 16,
+    fontSize: 18,
+  },
+  btnText: {
+    color: "#FFF",
+    fontSize: 36,
+  },
+  addBtn: {
+    width: 56,
+    height: 56,
+    borderRadius: 5,
+    backgroundColor: "#31cf67",
+    alignItems: "center",
+    justifyContent: "center",
+    marginLeft: 10,
+  },
+  form: {
+    width: "100%",
+    flexDirection: "row",
+    marginTop: 36,
+  },
+});
